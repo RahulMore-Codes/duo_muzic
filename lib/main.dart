d1e67b47bf5e3b53a1f51776723539ecebe0d0e9
RahulMore-Codes <dev.rahulmore@gmail.com>
2021-01-23 19:46:51 +0530

    Starting developing again...

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Duo Muzic",
       theme: ThemeData(
         primaryColor: Colors.white,
